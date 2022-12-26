db55834d4a8bc1b624a2387f997f0e8b7c9208ab
Patrick-McGuire <patrick.manson.mcguire@gmail.com>
2022-12-25 21:20:43 -0500

    Added todo

--- QcdExample.cpp
+++ QcdExample.cpp
@@ -22,37 +22,6 @@
 #include "src/Util/VideoRecorder.h"
 #include "opencv2/opencv.hpp"
 
-/*
- * TODO: Future work (no particular order):
- * --------------- Front end
- * Right click menu editing
- * Settings menu
- * Re-do stylesheet stuff
- * Figure out size policy/hints
- * --------------- Back end
- * Config Loading, and OS specific config stuff
- * Look into subscriber/publisher direct connection
- * Connect menus to internal event structure (base menu & base action class?)
- * --------------- Hard/separate
- * Python and C++ libs for communicating over TCP
- * Map vis
- * 3D vis
- * Windows compatibility
- * Mac compatibility
- * Figure out how to do multiple build configurations within same project
- * --------------- Maintenance
- * Finish event based video recording architecture, add timer to recorder widget
- * Finish update rate scaling
- * Add get/set methods to all widgets
- * Doxygen comments
- * Change updateTheme signal to use internal event format
- * --------------- Implementations
- * GUIManagers: Base, (XML, ROS, FRC dash, Example)
- * Port widgets: (Basic charts, annunciator, compas, nav ball, ros/dropdown)
- * New widgets: (static plot)
- * Interfaces: (ROS interface, TCP interface, Serial interface)
- */
-
 
 int main(int argc, char **argv) {
 
---
+++ TODO.md
@@ -0,0 +1,28 @@
+# Outstanding work
+## Front end
+1) Right click menu editing
+2) Settings menu
+3) Re-do stylesheet stuff
+4) Figure out size policy/hints
+## Back end
+5) Config Loading, and OS specific config stuff
+6) Look into subscriber/publisher direct connection
+7) Connect menus to internal event structure (base menu & base action class?)
+## Hard/separate
+8) Python and C++ libs for communicating over TCP
+9) Map vis
+10) 3D vis
+11) Windows compatibility
+12) Mac compatibility
+13) ~~Figure out how to do multiple build configurations within same project~~
+## Maintenance
+14) Finish event based video recording architecture, add timer to recorder widget 
+15) Finish update rate scaling
+16) Add get/set methods to all widgets
+17) Doxygen comments
+18) Change updateTheme signal to use internal event format
+## Implementations
+19) GUIManagers: Base, (XML, ROS, FRC dash, Example)
+20) Port widgets: (Basic charts, annunciator, compas, nav ball, ros/dropdown)
+21) New widgets: (static plot)
+22) Interfaces: (ROS interface, TCP interface, Serial interface)
